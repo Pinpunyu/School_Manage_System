e1043cee9764e97c3bf7497e4ed06595fbf23b07
Pinpunyu <pe25617259@gmail.com>
2022-12-29 02:26:22 +0800

    add SQL injection

--- apply_data.php
+++ apply_data.php
@@ -1,38 +1,16 @@
 <?php
 
+    session_start();
+    require_once('connect_sql.php');
+    $conn = connect_sql();
+    $account = $_SESSION['account'];
 
-session_start();
-require_once('conn.php');
+    //新增申請資料
+    $root = '3'; //放root權限，system menager更改時再update
+    $sql = "INSERT INTO Apply_Data(academic_year, semester, state, pay_fee_or_not, progress, student_account, system_manager_account) VALUES(?, ?, '審核中','not','', ?,?);";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("isss", $_POST["academic_year"], $_POST["semester"], $account, $root);
+    $stmt->execute();
 
-$academic_year = $_POST["academic_year"];
-$semester = $_POST["semester"];
-$apply_date = $_POST["apply_date"];
-$state = $_POST["state"];
-$pay_fee_or_not = $_POST["pay_fee_or_not"];
-$progress = $_POST["progress"];
-$apply_data_id = $_POST["apply_data_id"];
-$student_account = $_POST["student_account"];
-$system_manager_account = $_POST["system_manager_account"];
-// $_POST['key-name'] 取得輸入的資料
-// 以 empty() 判斷值是否為 null
-// if (empty($_POST['username'])) {
-//     // 中斷後面程序並顯示內容，() 內可輸入訊息字串
-//     die('請輸入 username');
-// }
-
-// sprintf() 裡面可以放入替代字元
-$sql = sprintf("INSERT INTO Apply_Data VALUES('%s','%d','%s','%s','%s','%s','%d','%s','%s')" 
-    ,$academic_year,$semester,$apply_date,$state,$pay_fee_or_not,$progress,$apply_data_id,$student_account,$system_manager_account);
-$result = mysqli_query($conn, $sql);
-// 確認是否有拿到結果
-if (!$result) {
-    die($conn->error);
-}
-
-header("Location: student.php"); // 自動跳轉回 index.php
+    header("Location: ./student.php"); // 自動跳轉回 student.php
 ?>
-
-<!-- 
-    echo $_POST["account"];
-    echo $_POST["password"];
--->
